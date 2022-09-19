25f26c1918f59445b9e3c902d4ea414a53128b7e
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-09-19 10:36:28 +0200

    added admin and user dashboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import LoginComponent from './components/login/LoginComponent'
+import LoginComponent from '../components/login/LoginComponent'
 
 const Home: NextPage = () => {
   return (
