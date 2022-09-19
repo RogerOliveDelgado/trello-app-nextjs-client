ce31c1e08ec3f98644ae6357a2f09d862149f4f7
juliomc23 <julito2302mc@gmail.com>
2022-09-19 15:03:57 +0200

    added styles to sidebar

--- components/sidebar/SidebarComponent.tsx
+++ components/sidebar/SidebarComponent.tsx
@@ -2,12 +2,7 @@ import * as React from "react";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import ListSubheader from "@mui/material/ListSubheader";
 import DashboardIcon from "@mui/icons-material/Dashboard";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import PeopleIcon from "@mui/icons-material/People";
-import BarChartIcon from "@mui/icons-material/BarChart";
-import LayersIcon from "@mui/icons-material/Layers";
 import AssignmentIcon from "@mui/icons-material/Assignment";
 import AddIcon from "@mui/icons-material/Add";
 import SettingsIcon from "@mui/icons-material/Settings";
@@ -21,6 +16,8 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 
+import styles from "../../styles/Dashboard.module.css";
+
 export default function SidebarComponent() {
   const [open, setOpen] = React.useState(false);
 
@@ -33,73 +30,72 @@ export default function SidebarComponent() {
   };
 
   return (
-    <div>
-      <React.Fragment>
+    <div className={styles.sidebar}>
+      <div className={styles.top}>
         <ListItemButton>
-          <ListItemIcon>
+          <ListItemIcon className={styles.buttons}>
             <HomeIcon />
           </ListItemIcon>
           <ListItemText primary="Home" />
         </ListItemButton>
         <ListItemButton>
-          <ListItemIcon>
+          <ListItemIcon className={styles.buttons}>
             <DashboardIcon />
           </ListItemIcon>
           <ListItemText primary="Dashboard" />
         </ListItemButton>
         <ListItemButton>
-          <ListItemIcon>
+          <ListItemIcon className={styles.buttons}>
             <AssignmentIcon />
           </ListItemIcon>
-          <ListItemText primary="Taks" />
+          <ListItemText primary="Task" />
         </ListItemButton>
-      </React.Fragment>
+      </div>
 
-      <React.Fragment>
+      <div className={styles.bot}>
         <ListItemButton onClick={handleClickOpen}>
-          <ListItemIcon>
+          <ListItemIcon className={styles.buttons}>
             <AddIcon />
           </ListItemIcon>
           <ListItemText primary="Create Board" />
         </ListItemButton>
         <ListItemButton>
-          <ListItemIcon>
+          <ListItemIcon className={styles.buttons}>
             <AddIcon />
           </ListItemIcon>
           <ListItemText primary="Create Task" />
         </ListItemButton>
         <ListItemButton>
-          <ListItemIcon>
+          <ListItemIcon className={styles.buttons}>
             <SettingsIcon />
           </ListItemIcon>
           <ListItemText primary="Settings" />
         </ListItemButton>
-      </React.Fragment>
+      </div>
+
       {open && (
-        <div>
-          <Dialog open={open} onClose={handleClose}>
-            <DialogTitle>Subscribe</DialogTitle>
-            <DialogContent>
-              <DialogContentText>
-                To subscribe to this website, please enter your email address
-                here. We will send updates occasionally.
-              </DialogContentText>
-              <TextField
-                autoFocus
-                margin="dense"
-                id="name"
-                label="Email Address"
-                type="email"
-                fullWidth
-                variant="standard"
-              />
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleClose}>Cancel</Button>
-              <Button onClick={handleClose}>Subscribe</Button>
-            </DialogActions>
-          </Dialog>
-        </div>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>Subscribe</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              To subscribe to this website, please enter your email address
+              here. We will send updates occasionally.
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Email Address"
+              type="email"
+              fullWidth
+              variant="standard"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handleClose}>Subscribe</Button>
+          </DialogActions>
+        </Dialog>
       )}
     </div>
   );
