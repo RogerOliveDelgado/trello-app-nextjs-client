cf4f8ac795ee3346df705f8c709724453a1984dd
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-09-20 14:31:24 +0200

    added Avatar to task card

--- components/taskCards/TaskCard.tsx
+++ components/taskCards/TaskCard.tsx
@@ -2,19 +2,36 @@ import * as React from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
+import { CardHeader } from "@mui/material";
+import Avatar from "@mui/material/Avatar";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import { textAlign } from "@mui/system";
 
 export default function TaskCard() {
   return (
-    <Card sx={{
-      maxWidth: 345,
-      margin: "1rem",
-    }}>
+    <Card
+      sx={{
+        maxWidth: 345,
+        margin: "1rem",
+        padding: ".5rem",
+      }}
+    >
       <CardContent>
+        <CardHeader
+          avatar={
+            <Avatar
+              sx={{
+                bgcolor: "blueviolet",
+              }}
+            >
+              J
+            </Avatar>
+          }
+        />
         <Typography gutterBottom variant="h5" component="div">
           Title task
         </Typography>
