5cfdc5371740ac165438db19c651bea2f65fbff0
Alejandro <alejandroaperez1994g@gmail.com>
2022-09-20 10:00:56 +0200

    feat: :sparkles: added getTasks functionality

---
+++ services/getTasks.tsx
@@ -0,0 +1,22 @@
+import axios, { AxiosResponse } from 'axios';
+import Task from '../interfaces/Task';
+
+type GetTasksResponse = {
+  data: Task[];
+};
+
+export const getTasks = async (token: string) => {
+  try {
+    const { data, status } = await axios.get<GetTasksResponse>(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/getTasks`,
+      {
+        headers: {
+          Authorization: `${token}`,
+        },
+      }
+    );
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
