233035779d1e73913bada940fe0fbaa818b102f2
Alejandro <alejandroaperez1994g@gmail.com>
2022-09-23 09:17:55 +0200

    feat: :sparkles: added fields to adduser modal

--- components/modals/confirmationModal/ConfirmationModal.tsx
+++ components/modals/confirmationModal/ConfirmationModal.tsx
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Button from '@mui/material/Button';
+import {
+  Grid,
+  TextField,
+  Select,
+  MenuItem,
+  SelectChangeEvent,
+  InputLabel,
+} from '@mui/material';
 
 type Props = {
   open: boolean;
@@ -11,25 +20,98 @@ type Props = {
 };
 
 const ConfirmationModal = ({ open, handleClose }: Props) => {
+  const [role, setRole] = useState('Admin');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setRole(event.target.value as string);
+  };
+
   return (
     <Dialog
       open={open}
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
     >
-      <DialogTitle id="alert-dialog-title">
-        {"Use Google's location service?"}
-      </DialogTitle>
+      <DialogTitle id="alert-dialog-title">{'Add new User'}</DialogTitle>
       <DialogContent>
         <DialogContentText id="alert-dialog-description">
-          Let Google help apps determine location. This means sending anonymous
-          location data to Google, even when no apps are running.
+          <Grid container spacing={1}>
+            <Grid item md={6} xs={12}>
+              <TextField
+                fullWidth
+                helperText="Please specify the first name"
+                label="First name"
+                name="firstName"
+                required
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item md={6} xs={12}>
+              <TextField
+                fullWidth
+                label="Last name"
+                name="lastName"
+                required
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item md={6} xs={12}>
+              <TextField
+                fullWidth
+                label="Address"
+                name="address"
+                required
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item md={6} xs={12}>
+              <TextField
+                fullWidth
+                label="Email"
+                name="email"
+                required
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item md={6} xs={12}>
+              <TextField
+                fullWidth
+                label="Password"
+                name="password"
+                type="password"
+                required
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item md={6} xs={12}>
+              <TextField
+                fullWidth
+                label="Birthday"
+                name="birthday"
+                type="date"
+                required
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item md={6} xs={12}>
+              <InputLabel id="demo-simple-select-label">Role</InputLabel>
+              <Select
+                labelId="select-role"
+                id="select-role"
+                label="Role"
+                value={role}
+              >
+                <MenuItem value={'admin'}>Admin</MenuItem>
+                <MenuItem value={'user'}>User</MenuItem>
+              </Select>
+            </Grid>
+          </Grid>
         </DialogContentText>
       </DialogContent>
       <DialogActions>
         <Button onClick={handleClose}>Cancel</Button>
         <Button onClick={handleClose} autoFocus>
-          Agree
+          Add
         </Button>
       </DialogActions>
     </Dialog>
