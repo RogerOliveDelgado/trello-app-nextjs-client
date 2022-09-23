e7032b6e8e247121441fefe63e3cebb0e534e02f
Alejandro <alejandroaperez1994g@gmail.com>
2022-09-23 16:12:40 +0200

    feat: :construction: create User

---
+++ services/createUser.ts
@@ -0,0 +1,19 @@
+type Options = {
+  method: string;
+  headers: {
+    'Content-Type': string;
+  };
+  body: string;
+};
+
+export const createUser = async (requestOptions: Options) => {
+  const req = await fetch(
+    'https://trello-app-express-server.vercel.app/auth/SignUp',
+    requestOptions
+  );
+  const res = await req.json();
+
+  console.log(res);
+
+  return res;
+};
